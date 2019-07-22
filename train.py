2ff856e6567a185fde0432f345b9e93085b7e5c7
Suhyun Kim <logicdevildotcom@gmail.com>
2019-07-22 17:21:31 -0400

    Replaced metrics with mean iou

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import numpy as np
 import glob
 import os
 import random
+import keras
 from keras.models import Model
 from keras.preprocessing.image import ImageDataGenerator
 from keras import backend as K
@@ -28,6 +29,32 @@ def dice_coef(y_true, y_pred):
 
 def dice_coef_loss(y_true, y_pred):
     return 1. - dice_coef(y_true, y_pred)
+
+
+# Custom IoU metric
+def mean_iou(y_true, y_pred):
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        y_pred_ = tf.to_int32(y_pred > t)
+        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([up_opt]):
+            score = tf.identity(score)
+        prec.append(score)
+    return K.mean(K.stack(prec), axis=0)
+
+
+# Custom loss function
+def dice_coef(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def bce_dice_loss(y_true, y_pred):
+    return 0.5 * keras.losses.binary_crossentropy(y_true, y_pred) - dice_coef(y_true, y_pred)
 
 
 def get_unet(img_rows, img_cols):
@@ -173,7 +200,7 @@ def get_unet(img_rows, img_cols):
     conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
 
     model = Model(inputs=inputs, outputs=conv10)
-    model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
+    model.compile(optimizer=Adam(lr=1e-5), loss=bce_dice_loss, metrics=[mean_iou])
 
     return model
 
